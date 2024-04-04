921be91338ce2711bf268d67ee9c4d14066e257b
RealGreed <lagraprimary@gmail.com>
2024-04-03 20:35:54 -0500

    fixed

--- bot.py
+++ bot.py
@@ -1,42 +1,41 @@
 from nextcord.ext import commands
-import requests, json, random, datetime, asyncio
+import nextcord
 import os
+import datetime
+import asyncio
 
 bot = commands.Bot(command_prefix="dog ")
 
 async def meeting_reminder():
-	channel = bot.get_channel(1039284243697762467)
-	await channel.send("@here MEETING IS IN 30 MINUTES!")
+    channel = bot.get_channel(1039284243697762467)
+    await channel.send("@here MEETING IS IN 30 MINUTES!")
 
-	
 async def meeting_message():
-	channel = bot.get_channel(1039284243697762467)
-
-	await channel.send("@here THE MEETING IS BEING HELD IN #MEETING!")
+    channel = bot.get_channel(1039284243697762467)
+    await channel.send("@here THE MEETING IS BEING HELD IN #MEETING!")
 
 async def check_time_and_send_messages():
-	now = datetime.datetime.now()
+    while True:
+        now = datetime.datetime.now()
 
-	# only runs on wednesdays
-	if now.weekday() != 2:
-		return
-	
-	reminder_time = now.replace(hour=20, minute=30)
-	meeting_time = now.replace(hour=21, minute=0)
-	
-	if now >= meeting_time:
-		await meeting_message()
-	elif now >= reminder_time:
-		await meeting_message()
-	else:
-		return
-	
+        # only runs on Wednesdays
+        if now.weekday() == 2:
+            reminder_time = now.replace(hour=20, minute=30)
+            meeting_time = now.replace(hour=21, minute=0)
+            
+            if now >= meeting_time:
+                await meeting_message()
+            elif now >= reminder_time:
+                await meeting_reminder()
+
+        # Sleep for 1 hour before checking again
+        await asyncio.sleep(3600)  # 3600 seconds = 1 hour
+
 @bot.event
 async def on_ready():
-	print(f"Loggined in as: {bot.user.name}")
-
-	bot.loop.create_task(check_time_and_send_messages())
-	
+    print(f"Logged in as: {bot.user.name}")
+    # Start the loop to check time and send messages
+    bot.loop.create_task(check_time_and_send_messages())
 
 if __name__ == '__main__':
-	bot.run(os.environ["DISCORD_TOKEN"])
+    bot.run(os.environ["DISCORD_TOKEN"])
